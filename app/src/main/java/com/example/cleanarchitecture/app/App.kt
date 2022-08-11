6927ad225f45034874ffd9d358ef1420e40c1e64
vlatrof <gthbttr@gmail.com>
2022-08-11 21:21:16 +0300

    dev

---
+++ app/src/main/java/com/example/cleanarchitecture/app/App.kt
@@ -0,0 +1,31 @@
+package com.example.cleanarchitecture.app
+
+import android.app.Application
+import com.example.cleanarchitecture.di.appModule
+import com.example.cleanarchitecture.di.dataModule
+import com.example.cleanarchitecture.di.domainModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger(Level.DEBUG)
+            androidContext(this@App)
+            modules(
+                listOf(
+                    appModule,
+                    domainModule,
+                    dataModule
+                )
+            )
+        }
+
+    }
+
+}
