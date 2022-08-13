a07c424e8d629a10c61901a8a433d766afe8121c
vlatrof <gthbttr@gmail.com>
2022-08-13 21:36:57 +0300

    dev

--- app/src/main/java/com/example/cleanarchitecture/di/DomainModule.kt
+++ app/src/main/java/com/example/cleanarchitecture/di/DomainModule.kt
@@ -1,7 +1,14 @@
 package com.example.cleanarchitecture.di
 
+import com.example.cleanarchitecture.domain.repositories.NoteRepository
+import com.example.cleanarchitecture.domain.usecases.SaveNoteUseCase
 import dagger.Module
 
 @Module
 class DomainModule {
+
+    fun provideSaveNoteUseCase(noteRepository: NoteRepository): SaveNoteUseCase {
+        return SaveNoteUseCase(noteRepository = noteRepository)
+    }
+
 }
