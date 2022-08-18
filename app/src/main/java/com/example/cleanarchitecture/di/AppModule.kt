75899f32ceca5731fe47cad3f1f0772e47468074
vlatrof <gthbttr@gmail.com>
2022-08-18 10:30:36 +0300

    test

--- app/src/main/java/com/example/cleanarchitecture/di/AppModule.kt
+++ app/src/main/java/com/example/cleanarchitecture/di/AppModule.kt
@@ -4,6 +4,8 @@ import com.example.cleanarchitecture.presentation.MainViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
+// hilt test
+
 val appModule = module {
 
     viewModel<MainViewModel> {
