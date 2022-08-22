b4f9e02866fe5a13ee26ebb4267c0f4ef046c8ab
vlatrof <gthbttr@gmail.com>
2022-08-22 19:46:30 +0300

    dev

---
+++ app/src/test/java/com/example/cleanarchitecture/presentation/MainViewModelTest.kt
@@ -0,0 +1,21 @@
+package com.example.cleanarchitecture.presentation
+
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Test
+
+class MainViewModelTest {
+
+    @AfterEach
+    fun tear
+
+    @Test
+    fun `should save note successfully`() {
+
+    }
+
+    @Test
+    fun `should load note successfully`() {
+
+    }
+
+}
